a42ab756d6048194669476c26da333b090165938
Nazar <nazar.usov92@gmail.com>
2022-04-18 16:54:08 +0300

    Add setting useTor and modal transaction status

--- html_source/src/app/app.component.ts
+++ html_source/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, NgZone, Renderer2, OnDestroy, ViewChild } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateService } from '@ngx-translate/core';
-import { BackendService } from './_helpers/services/backend.service';
+import { AsyncCommandResults, BackendService } from './_helpers/services/backend.service';
 import { Router } from '@angular/router';
 import { VariablesService } from './_helpers/services/variables.service';
 import { ContextMenuComponent } from 'ngx-contextmenu';
@@ -10,6 +10,7 @@ import { BigNumber } from 'bignumber.js';
 import { ModalService } from './_helpers/services/modal.service';
 import { UtilsService } from './_helpers/services/utils.service';
 import { Store } from 'store';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -27,6 +28,8 @@ export class AppComponent implements OnInit, OnDestroy {
   translateUsed = false;
 
   needOpenWallets = [];
+
+  private _destroy$: Subject<never> = new Subject<never>();
 
   @ViewChild('allContextMenu') public allContextMenu: ContextMenuComponent;
   @ViewChild('onlyCopyContextMenu') public onlyCopyContextMenu: ContextMenuComponent;
@@ -171,7 +174,6 @@ export class AppComponent implements OnInit, OnDestroy {
           });
         }
       });
-
 
 
       this.backend.eventSubscribe('update_daemon_state', (data) => {
@@ -381,7 +383,7 @@ export class AppComponent implements OnInit, OnDestroy {
         console.log('----------------- handle_deeplink_click -----------------');
         console.log(data);
         if (data) {
-          this.variablesService.deeplink$.next(data)
+          this.variablesService.deeplink$.next(data);
         }
       });
 
@@ -545,7 +547,6 @@ export class AppComponent implements OnInit, OnDestroy {
         });
       });
 
-
       this.backend.getAppData((status, data) => {
         if (data && Object.keys(data).length > 0) {
           for (const key in data) {
@@ -559,7 +560,7 @@ export class AppComponent implements OnInit, OnDestroy {
             this.renderer.setStyle(app, 'min-width', width + 'px');
             this.renderer.setStyle(document.documentElement, 'font-size', this.variablesService.settings.scale + 'px');
           } else {
-            this.variablesService.settings.scale = 8
+            this.variablesService.settings.scale = 8;
             const width = this.utilsService.getMinWidthByScale(this.variablesService.settings.scale);
             const app = document.documentElement.querySelector('app-root');
             this.renderer.setStyle(app, 'min-width', width + 'px');
@@ -570,12 +571,13 @@ export class AppComponent implements OnInit, OnDestroy {
         this.setBackendLocalization();
 
         this.backend.setLogLevel(this.variablesService.settings.appLog);
+        this.backend.setEnableTor(this.variablesService.settings.appUseTor);
 
         if (this.router.url !== '/login') {
           this.backend.haveSecureAppData((statusPass) => {
             if (statusPass) {
               this.ngZone.run(() => {
-                this.router.navigate(['/login'], { queryParams: { type: 'auth' } });
+                this.router.navigate(['/login'], {queryParams: {type: 'auth'}});
               });
             } else {
               if (Object.keys(data).length !== 0) {
@@ -586,11 +588,21 @@ export class AppComponent implements OnInit, OnDestroy {
                 });
               } else {
                 this.ngZone.run(() => {
-                  this.router.navigate(['/login'], { queryParams: { type: 'reg' } });
+                  this.router.navigate(['/login'], {queryParams: {type: 'reg'}});
                 });
               }
             }
           });
+        }
+      });
+
+      /** Listening dispatchAsyncCallResult */
+      this.backend.dispatchAsyncCallResult((asyncCommandResults: AsyncCommandResults) => {
+        const {command, response} = asyncCommandResults;
+        const {appUseTor} = this.variablesService.settings;
+        if (command === 'transfer' && response) {
+          const {response_data: {success}} = response;
+          return appUseTor || success && this.modalService.prepareModal('success', 'SEND.SUCCESS_SENT');
         }
       });
     }, error => {
@@ -609,7 +621,7 @@ export class AppComponent implements OnInit, OnDestroy {
         this.http.get('https://api.coingecko.com/api/v3/simple/price?ids=zano&vs_currencies=usd&include_24hr_change=true').subscribe(
           data => {
             this.variablesService.moneyEquivalent = data['zano']['usd'];
-            this.variablesService.moneyEquivalentPercent = data['zano']["usd_24h_change"];
+            this.variablesService.moneyEquivalentPercent = data['zano']['usd_24h_change'];
           },
           error => {
             console.warn('api.coingecko.com price error: ', error);
@@ -622,7 +634,7 @@ export class AppComponent implements OnInit, OnDestroy {
           this.getMoneyEquivalent();
         }, 30000);
       }
-    )
+    );
   }
 
   getAliases() {
@@ -731,22 +743,23 @@ export class AppComponent implements OnInit, OnDestroy {
       if (sync && sync.length) {
         const result = value.map(item => {
           if (item.wallet_id === wallet.wallet_id) {
-            return { sync: boolean, wallet_id: wallet.wallet_id };
+            return {sync: boolean, wallet_id: wallet.wallet_id};
           } else {
             return item;
           }
         });
         this.store.set('sync', result);
       } else {
-        value.push({ sync: boolean, wallet_id: wallet.wallet_id });
+        value.push({sync: boolean, wallet_id: wallet.wallet_id});
         this.store.set('sync', value);
       }
     } else {
-      this.store.set('sync', [{ sync: boolean, wallet_id: wallet.wallet_id }]);
+      this.store.set('sync', [{sync: boolean, wallet_id: wallet.wallet_id}]);
     }
   }
 
   ngOnDestroy() {
+    this._destroy$.next();
     if (this.intervalUpdateContractsState) {
       clearInterval(this.intervalUpdateContractsState);
     }
--- html_source/src/app/send-modal/send-modal.component.ts
+++ html_source/src/app/send-modal/send-modal.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { VariablesService } from '../_helpers/services/variables.service';
-
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {VariablesService} from '../_helpers/services/variables.service';
 
 @Component({
   selector: 'app-send-modal',
@@ -10,6 +9,7 @@ import { VariablesService } from '../_helpers/services/variables.service';
 })
 export class SendModalComponent implements OnInit {
   @Input() form: FormGroup;
+
   @Output() confirmed: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   confirmForm = new FormGroup({
@@ -29,17 +29,17 @@ export class SendModalComponent implements OnInit {
   }
 
 
-  confirm() {
+  submit() {
     if (this.variablesService.appPass) {
       if (this.confirmForm.controls['password'].value === '') {
-        this.confirmForm.controls['password'].setErrors({ requiredPass: true });
+        this.confirmForm.controls['password'].setErrors({requiredPass: true});
         return;
       }
-      this.confirmForm.controls['password'].setErrors({ requiredPass: false });
+      this.confirmForm.controls['password'].setErrors({requiredPass: false});
       if (this.variablesService.appPass === this.confirmForm.controls['password'].value) {
         this.confirmed.emit(true);
       } else {
-        this.confirmForm.controls['password'].setErrors({ passwordNotMatch: true })
+        this.confirmForm.controls['password'].setErrors({passwordNotMatch: true});
       }
     } else {
       this.confirmed.emit(true);
