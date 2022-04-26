e7ec661f58479caed72a29e06d9ab18a888a918b
Nazar <nazar.usov92@gmail.com>
2022-04-26 17:24:34 +0300

    delete spec

--- html_source/src/app/_shared/directives/disable-price-fetch/disable-price-fetch.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisablePriceFetchDirective } from './disable-price-fetch.directive';
-
-describe('DisablePriceFetchDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DisablePriceFetchDirective();
-    expect(directive).toBeTruthy();
-  });
-});
