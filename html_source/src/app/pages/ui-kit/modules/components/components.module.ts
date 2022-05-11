4855f4ab3fd6be50a3a6b236868f5d0e1a94d474
Nazar <nazar.usov92@gmail.com>
2022-05-11 15:52:39 +0300

    update ui-kit

--- html_source/src/app/pages/ui-kit/modules/components/components.module.ts
+++ html_source/src/app/pages/ui-kit/modules/components/components.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonsComponent, ProgressBarsComponent } from './';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { WalletsComponent } from './wallets/wallets.component';
+import { SharedModule } from '../../../../_shared/shared.module';
 
 @NgModule({
-  declarations: [ButtonsComponent, ProgressBarsComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [ButtonsComponent, ProgressBarsComponent]
+  declarations: [ButtonsComponent, ProgressBarsComponent, BreadcrumbsComponent, WalletsComponent],
+            imports: [
+              CommonModule,
+              TranslateModule,
+              SharedModule
+            ],
+  exports: [ButtonsComponent, ProgressBarsComponent, BreadcrumbsComponent, WalletsComponent]
 })
 export class ComponentsModule { }
