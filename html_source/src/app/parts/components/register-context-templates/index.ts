09387c2ba89334116a9a01ea55a018313d5a5924
Nazar <nazar.usov92@gmail.com>
2022-12-08 18:27:25 +0200

    Update register context. Update template for components.

---
+++ html_source/src/app/parts/components/register-context-templates/index.ts
@@ -0,0 +1,2 @@
+export * from './register-context-templates.component';
+export * from './register-context-templates.module';
---
+++ html_source/src/app/parts/components/register-context-templates/register-context-templates.component.ts
@@ -0,0 +1,133 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ContextMenuComponent } from '@perfectmemory/ngx-contextmenu';
+import { VariablesService } from '@parts/services/variables.service';
+import { BackendService } from '@api/services/backend.service';
+
+@Component({
+  selector: 'app-register-context-templates',
+  template: ` <context-menu #allContextMenu>
+      <ng-template (execute)="contextMenuCopy($event.value)" contextMenuItem>{{
+        'CONTEXT_MENU.COPY' | translate
+      }}</ng-template>
+      <ng-template (execute)="contextMenuPaste($event.value)" contextMenuItem>{{
+        'CONTEXT_MENU.PASTE' | translate
+      }}</ng-template>
+      <ng-template
+        (execute)="contextMenuSelect($event.value)"
+        contextMenuItem
+        >{{ 'CONTEXT_MENU.SELECT' | translate }}</ng-template
+      >
+    </context-menu>
+    <context-menu #onlyCopyContextMenu>
+      <ng-template
+        (execute)="contextMenuOnlyCopy($event.value)"
+        contextMenuItem
+        >{{ 'CONTEXT_MENU.COPY' | translate }}</ng-template
+      >
+    </context-menu>
+
+    <context-menu #pasteSelectContextMenu>
+      <ng-template (execute)="contextMenuPaste($event.value)" contextMenuItem>{{
+        'CONTEXT_MENU.PASTE' | translate
+      }}</ng-template>
+      <ng-template
+        (execute)="contextMenuSelect($event.value)"
+        contextMenuItem
+        >{{ 'CONTEXT_MENU.SELECT' | translate }}</ng-template
+      >
+    </context-menu>`,
+})
+export class RegisterContextTemplatesComponent implements OnInit {
+  @ViewChild('allContextMenu', { static: true })
+  public allContextMenu: ContextMenuComponent<any>;
+
+  @ViewChild('onlyCopyContextMenu', { static: true })
+  public onlyCopyContextMenu: ContextMenuComponent<any>;
+
+  @ViewChild('pasteSelectContextMenu', { static: true })
+  public pasteSelectContextMenu: ContextMenuComponent<any>;
+
+  constructor(
+    public variablesService: VariablesService,
+    private backendService: BackendService
+  ) {}
+
+  ngOnInit(): void {
+    this.variablesService.allContextMenu = this.allContextMenu;
+    this.variablesService.onlyCopyContextMenu = this.onlyCopyContextMenu;
+    this.variablesService.pasteSelectContextMenu = this.pasteSelectContextMenu;
+  }
+
+  contextMenuCopy(target): void {
+    if (
+      target &&
+      (target['nodeName'].toUpperCase() === 'TEXTAREA' ||
+        target['nodeName'].toUpperCase() === 'INPUT')
+    ) {
+      const start = target['contextSelectionStart']
+        ? 'contextSelectionStart'
+        : 'selectionStart';
+      const end = target['contextSelectionEnd']
+        ? 'contextSelectionEnd'
+        : 'selectionEnd';
+      const canUseSelection = target[start] || target[start] === '0';
+      const SelectedText = canUseSelection
+        ? target['value'].substring(target[start], target[end])
+        : target['value'];
+      this.backendService.setClipboard(String(SelectedText));
+    }
+  }
+
+  contextMenuOnlyCopy(text): void {
+    if (text) {
+      this.backendService.setClipboard(String(text));
+    }
+  }
+
+  contextMenuPaste(target): void {
+    if (
+      target &&
+      (target['nodeName'].toUpperCase() === 'TEXTAREA' ||
+        target['nodeName'].toUpperCase() === 'INPUT')
+    ) {
+      this.backendService.getClipboard((status, clipboard) => {
+        clipboard = String(clipboard);
+        if (typeof clipboard !== 'string' || clipboard.length) {
+          const start = target['contextSelectionStart']
+            ? 'contextSelectionStart'
+            : 'selectionStart';
+          const end = target['contextSelectionEnd']
+            ? 'contextSelectionEnd'
+            : 'selectionEnd';
+          const _pre = target['value'].substring(0, target[start]);
+          const _aft = target['value'].substring(
+            target[end],
+            target['value'].length
+          );
+          let text = _pre + clipboard + _aft;
+          const cursorPosition = (_pre + clipboard).length;
+          if (target['maxLength'] && parseInt(target['maxLength'], 10) > 0) {
+            text = text.substr(0, parseInt(target['maxLength'], 10));
+          }
+          target['value'] = text;
+          target.setSelectionRange(cursorPosition, cursorPosition);
+          target.dispatchEvent(new Event('input'));
+          target['focus']();
+        }
+      });
+    }
+  }
+
+  contextMenuSelect(target): void {
+    if (
+      target &&
+      (target['nodeName'].toUpperCase() === 'TEXTAREA' ||
+        target['nodeName'].toUpperCase() === 'INPUT')
+    ) {
+      target['focus']();
+      setTimeout(() => {
+        target['select']();
+      });
+    }
+  }
+}
