baffd355f4459d5c5fa7542b0cd9d801c8df98c8
Nazar <nazar.usov92@gmail.com>
2023-06-02 10:39:01 +0300

    Hot fix for send tab

--- html_source/src/app/parts/data/assets.ts
+++ html_source/src/app/parts/data/assets.ts
@@ -1,7 +1,7 @@
 import { WhiteAssetInfo } from '@api/models/assets.model';
 
 export const zanoAssetInfo: WhiteAssetInfo = {
-  asset_id: '0000000000000000000000000000000000000000000000000000000000000000',
+  asset_id: 'd6329b5b1f7c0805b5c345f4957554002a2f557845f64d7645dae0e051a6498a',
   current_supply: 0,
   decimal_point: 12,
   full_name: 'Zano',
