98d880380ff45257f23336841281147dd0807a63
Nazar <nazar.usov92@gmail.com>
2023-06-21 10:52:20 +0300

    Updated git actions

--- html/polyfills.js
+++ html/polyfills.js
@@ -5800,8 +5800,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/runner/work/zano_ui/zano_ui/html_source/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /home/runner/work/zano_ui/zano_ui/html_source/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! D:\Work\Ristars\Nazar\zano_ui\html_source\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! D:\Work\Ristars\Nazar\zano_ui\html_source\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
