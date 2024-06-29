e5ce696b04000a07a94fb492c9863bd4cdcba8a9
Nazar Usov <nazarusov@Notebook.local>
2024-06-29 13:48:02 +0300

    build html

--- html_source/src/main.ts
+++ html_source/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import 'lodash';
 
-export const buildTime = '2024-03-06T13:59:36.086Z';
+export const buildTime = '2024-06-29T10:47:09.911Z';
 
 if (environment.production) {
     enableProdMode();
