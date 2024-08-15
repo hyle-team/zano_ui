040a0515b33a059f020f08b4c58a6976b0eda8f5
Nazar Usov <nazarusov@Notebook.local>
2024-08-15 15:25:57 +0300

    Update

--- html_source/src/app/pages/auth/login/login.component.ts
+++ html_source/src/app/pages/auth/login/login.component.ts
@@ -406,15 +406,13 @@ export class LoginComponent implements OnInit, OnDestroy {
 
     closeAllWallets(): void {
         this.variablesService.wallets.forEach(({ wallet_id }) => this.closeWallet(wallet_id));
-        if (this.variablesService.appPass) {
-            this.backend.storeSecureAppData();
-        }
     }
 
     closeWallet(wallet_id) {
         this.backend.closeWallet(wallet_id, () => {
             for (let i = this.variablesService.wallets.length - 1; i >= 0; i--) {
                 this.variablesService.wallets.splice(i, 1);
+                this.backend.storeSecureAppData();
             }
         });
     }
