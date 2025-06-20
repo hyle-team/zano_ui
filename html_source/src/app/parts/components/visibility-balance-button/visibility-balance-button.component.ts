3789d6e31e20fb6cc2622a6a5eb4e541464f1e50
Nazar Usov <nazarusov@Notebook.local>
2025-06-20 16:54:02 +0300

    Added tooltip for show/hide balance

--- html_source/src/app/parts/components/visibility-balance-button/visibility-balance-button.component.ts
+++ html_source/src/app/parts/components/visibility-balance-button/visibility-balance-button.component.ts
@@ -1,23 +1,17 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { VariablesService } from '@parts/services/variables.service';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'zano-visibility-balance-button',
     standalone: true,
-    imports: [CommonModule, MatIconModule],
+    imports: [CommonModule, MatIconModule, MatTooltipModule, TranslateModule],
     templateUrl: './visibility-balance-button.component.html',
     styleUrls: ['./visibility-balance-button.component.scss']
 })
-export class VisibilityBalanceButtonComponent implements OnInit {
+export class VisibilityBalanceButtonComponent {
     public readonly variablesService: VariablesService = inject(VariablesService);
-
-    constructor() {}
-
-    ngOnInit(): void {}
-
-    toggle(): void {
-        this.variablesService.visibilityBalance$.next(!this.variablesService.visibilityBalance$.value);
-    }
 }
